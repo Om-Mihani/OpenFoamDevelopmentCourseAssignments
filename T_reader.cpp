f9915d15cb0a76e4aa5558a15e2f8f9198e0e2f5
Om Mihani <cfd.om.mihani@gmail.com>
2022-05-09 13:40:02 +0530

    First commit

---
+++ T_reader.cpp
@@ -0,0 +1,42 @@
+#include "fvCFD.H"
+int main(int argc, char *argv[])
+{
+    Foam::argList::noBanner();
+    Foam::argList args(argc, argv)
+
+    Foam::Time runtime //creating a runTime object
+    (
+        Foam::Time::controlDictName,
+        args.rootPath(),
+        args.caseName()
+    );
+
+    Foam::fvMesh mesh // creating a mesh object
+    (
+        Foam::IOobject
+        (
+            Foam::fvMesh::defaultRegion,
+            runtime.timeName(),
+            runTime,
+            Foam::IOobject::MUST_READ
+        )
+    );
+
+    IOdictionary ioDictObj
+    (
+        IOobject //Creating an IO object
+        (
+            "T",
+            "thermalProperties",
+            runTime,
+            IOobject::MUST_READ,
+            IOobject::No_WRITE
+        );
+
+        Scalar T = scalar(ioDictObj.lookup("T"));
+
+        Info<<T<<"\n";
+    )
+
+
+}
